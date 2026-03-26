9484ee5fc59c61d33462915f325d8e2347eb58b1
Minh <minhvip1982004@gmail.com>
2026-03-26 15:01:27 +0700

    feat: new

---
+++ src/main/java/com/example/laivuhoangminh/LaivuhoangminhApplication.java
@@ -0,0 +1,13 @@
+package com.example.laivuhoangminh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaivuhoangminhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaivuhoangminhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laivuhoangminh/LaivuhoangminhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laivuhoangminh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaivuhoangminhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
