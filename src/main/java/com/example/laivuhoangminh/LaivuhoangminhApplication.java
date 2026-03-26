a5ea579008b5cf6e30a69551a2130eb9761ffaa2
Minh <minhvip1982004@gmail.com>
2026-03-26 15:05:00 +0700

    feat: new

--- src/main/java/com/example/laivuhoangminh/LaivuhoangminhApplication.java
+++ src/main/java/com/example/laivuhoangminh/LaivuhoangminhApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LaivuhoangminhApplication {
-
+// aaaaaaaaaaaaa
 	public static void main(String[] args) {
 		SpringApplication.run(LaivuhoangminhApplication.class, args);
 	}
